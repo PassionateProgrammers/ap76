d958b9b849416fa0d4482cff63d5b12cc19f923e
PassionateProgrammers <jameskirk561@gmail.com>
2024-02-26 22:50:45 -0500

    mobile menu fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,8 @@ const Navbar = () => {
 
     const location = useLocation();
 
+    const handleMenuItemClick = () => setNav(false);
+
   return (
     <div className='fixed z-50 text-[#11203F] font-semibold bg-[#f5f5f5] w-full h-[75px] sm:h-[100px] flex justify-between items-center px-8 sm:px-20'>
         <div className='flex items-center'>
@@ -23,23 +25,23 @@ const Navbar = () => {
             </a>
         </div>
         <ul className='hidden lg:flex'>
-            <li className={location.pathname === '/' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/' ? 'active' : ''}>
                 <a href="/#/">Home</a>
             </li>
-            <li className={location.pathname === '/news' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/news' ? 'active' : ''}>
                 <a href='/#/news'>News</a>
             </li>
-            <li className={location.pathname === '/mission' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/mission' ? 'active' : ''}>
                 <a href="/#/what-we-do">What We Do</a>
             </li>
-            <li className={location.pathname === '/about' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/about' ? 'active' : ''}>
                 <a href="/#/about">About</a>
             </li>
-            <li className={location.pathname === '/contact' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/contact' ? 'active' : ''}>
                 <a href="/#/contact">Contact</a>
             </li>
-            <li>
-                <a href="/">Donate</a>
+            <li onClick={handleMenuItemClick}>
+                <a href="/#/">Donate</a>
             </li>
         </ul>
 
@@ -52,22 +54,22 @@ const Navbar = () => {
 
         {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-white text-4xl flex flex-col justify-center items-center space-y-8'}>
-            <li>
+            <li onClick={handleMenuItemClick}>
                 <a href="/#/">Home</a>
             </li>
-            <li className={location.pathname === '/news' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/news' ? 'active' : ''}>
                 <a href='/#/news'>News</a>
             </li>
-            <li className={location.pathname === '/mission' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/mission' ? 'active' : ''}>
                 <a href="/#/what-we-do">What We Do</a>
             </li>
-            <li className={location.pathname === '/about' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/about' ? 'active' : ''}>
                 <a href="/#/about">About</a>
             </li>
-            <li className={location.pathname === '/contact' ? 'active' : ''}>
+            <li onClick={handleMenuItemClick} className={location.pathname === '/contact' ? 'active' : ''}>
                 <a href="/#/contact">Contact</a>
             </li>
-            <li>
+            <li onClick={handleMenuItemClick}>
                 <a href="/#/">Donate</a>
             </li>
         </ul>
